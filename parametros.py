51b30299355efbcfa6c49d1e27f0a55b390f5f8e
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2023-12-14 13:22:59 +0100

    Add files via upload

---
+++ parametros.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+caja_iteraciones = 0
+caja_cerca = 0
+caja_media = 0
+caja_lejos = 0
+caja_minpuntos = 0
+caja_maxpuntos= 0
+boton_conectar= 0
+caja_umbral = 0
