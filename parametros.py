00814f93f1e6c3815b346d360719550a798e339d
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2023-12-14 13:23:39 +0100

    Update parametros.py

--- parametros.py
+++ parametros.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-caja_iteraciones = 0
-caja_cerca = 0
-caja_media = 0
-caja_lejos = 0
-caja_minpuntos = 0
-caja_maxpuntos= 0
-boton_conectar= 0
-caja_umbral = 0
+import tkinter as tk
+
+iteraciones = tk.StringVar()
+cerca = tk.StringVar()
+media = tk.StringVar()
+lejos = tk.StringVar()
+min_puntos = tk.StringVar()
+max_puntos = tk.StringVar()
+umbral = tk.StringVar()
