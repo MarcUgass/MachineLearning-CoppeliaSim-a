4b8bae74784972e47f14ab3e13cab0d2630b8c93
MarcUgass <93116190+MarcUgass@users.noreply.github.com>
2023-12-19 22:38:19 +0100

    Update clasificarSVM.py

--- clasificarSVM.py
+++ clasificarSVM.py
@@ -1,22 +1,28 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score, GridSearchCV
-import pickle
 from warnings import simplefilter
 
 # Ignorar futuras y advertencias de depreciación
 simplefilter(action='ignore', category=FutureWarning)
 simplefilter(action='ignore', category=DeprecationWarning)
 
-# Función para cargar los datos desde un archivo json
+# Función para cargar los datos desde un archivo JSON
 def cargar_datos(caracteristicas_file):
     caracteristicas_data = pd.read_json(caracteristicas_file, lines=True)
-    X = caracteristicas_data[['perímetro', 'profundidad', 'anchura']]
-    y = caracteristicas_data['esPierna']
+
+    # Obtener todos los atributos que no son 'numero_cluster'
+    X = caracteristicas_data.drop(columns=['numero_cluster', 'numero_puntos', 'perímetro', 'profundidad', 'anchura'])
+
+    # Usar 'numero_cluster' como etiqueta si existe
+    if 'numero_cluster' in caracteristicas_data:
+        y = caracteristicas_data['numero_cluster']
+    else:
+        y = None
+
     return X, y
 
 # Función para dividir los datos en conjuntos de entrenamiento y prueba
@@ -55,20 +61,8 @@ def grid_search_rbf(X_train, y_train):
     print(clf.best_estimator_)
     return clf.best_estimator_
 
-# Función para guardar el clasificador en un archivo
-def guardar_clasificador(clasificador, filename="clasificador.pkl"):
-    with open(filename, "wb") as archivo:
-        pickle.dump(clasificador, archivo)
-    print(f"Clasificador guardado en {filename}")
-
-# Función para cargar el clasificador desde un archivo
-def cargar_clasificador(filename="clasificador.pkl"):
-    with open(filename, "rb") as archivo:
-        clasificador = pickle.load(archivo)
-    return clasificador
-
 # Función principal
-def main():
+def entrenar():
     # Archivos de datos
     caracteristicas_file_piernas = "caracteristicasPiernas.dat"
     caracteristicas_file_no_piernas = "caracteristicasNoPiernas.dat"
@@ -86,52 +80,11 @@ def main():
 
     for kernel in kernels:
         print("\n== Clasificación de Piernas ==")
-        svc_piernas = clasificar_y_evaluar(kernel, X_train_piernas, X_test_piernas, y_train_piernas, y_test_piernas)
+        _ = clasificar_y_evaluar(kernel, X_train_piernas, X_test_piernas, y_train_piernas, y_test_piernas)
 
         print("\n== Clasificación de No Piernas ==")
-        svc_no_piernas = clasificar_y_evaluar(kernel, X_train_no_piernas, X_test_no_piernas, y_train_no_piernas, y_test_no_piernas)
+        _ = clasificar_y_evaluar(kernel, X_train_no_piernas, X_test_no_piernas, y_train_no_piernas, y_test_no_piernas)
 
     print("\n== Búsqueda de parámetros en un rango en el caso de RBF ==")
-    mejor_svc_rbf_piernas = grid_search_rbf(X_train_piernas, y_train_piernas)
-    mejor_svc_rbf_no_piernas = grid_search_rbf(X_train_no_piernas, y_train_no_piernas)
-
-    # Guardar clasificadores
-    guardar_clasificador(svc_piernas, "clasificador_piernas.pkl")
-    guardar_clasificador(svc_no_piernas, "clasificador_no_piernas.pkl")
-    guardar_clasificador(mejor_svc_rbf_piernas, "clasificador_mejor_rbf_piernas.pkl")
-    guardar_clasificador(mejor_svc_rbf_no_piernas, "clasificador_mejor_rbf_no_piernas.pkl")
-
-    # Cargar clasificadores
-    clasificador_piernas = cargar_clasificador("clasificador_piernas.pkl")
-    clasificador_no_piernas = cargar_clasificador("clasificador_no_piernas.pkl")
-    clasificador_mejor_rbf_piernas = cargar_clasificador("clasificador_mejor_rbf_piernas.pkl")
-    clasificador_mejor_rbf_no_piernas = cargar_clasificador("clasificador_mejor_rbf_no_piernas.pkl")
-
-    # Ejemplo de predicción
-    print("\n== Ejemplo de Predicción con Clasificador de Piernas ==")
-    ejemplo_piernas = np.array([[0.23, 0.12, 0.15]])
-    prediccion_piernas = clasificador_piernas.predict(ejemplo_piernas)
-    print(f"Predicción: {prediccion_piernas}")
-
-    # Ejemplo de predicción
-    print("\n== Ejemplo de Predicción con Clasificador de No Piernas ==")
-    ejemplo_no_piernas = np.array([[0.45, 0.25, 0.30]])
-    prediccion_no_piernas = clasificador_no_piernas.predict(ejemplo_no_piernas)
-    print(f"Predicción: {prediccion_no_piernas}")
-
-    # Ejemplo de predicción
-    print("\n== Ejemplo de Predicción con Clasificador RBF de Piernas ==")
-    ejemplo_rbf_piernas = np.array([[0.35, 0.20, 0.25]])
-    prediccion_rbf_piernas = clasificador_mejor_rbf_piernas.predict(ejemplo_rbf_piernas)
-    print(f"Predicción: {prediccion_rbf_piernas}")
-
-    # Ejemplo de predicción
-    print("\n== Ejemplo de Predicción con Clasificador RBF de No Piernas ==")
-    ejemplo_rbf_no_piernas = np.array([[0.55, 0.30, 0.35]])
-    prediccion_rbf_no_piernas = clasificador_mejor_rbf_no_piernas.predict(ejemplo_rbf_no_piernas)
-    print(f"Predicción: {prediccion_rbf_no_piernas}")
-
-
-if __name__ == "__main__":
-    main()
-
+    _ = grid_search_rbf(X_train_piernas, y_train_piernas)
+    
